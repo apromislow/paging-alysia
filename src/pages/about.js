d0c9591ee856fa04b52a4e870876f5dc59c9f773
apromismac <apromislow@gmail.com>
2021-11-09 00:06:14 -0600

    typo

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,7 @@ const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
       <p>My name is Alysia.  I graduated from N.C. State with ungergraduate degrees in Mathematics and Computer Science. Currently I'm employeed as a software developer at Epic Systems in Madison, WI.</p>
-      <p>My hobbies and interests include: cross country skiing, philosophy, backpacking, classic literature, and cryptcurrency.</p>
+      <p>My hobbies and interests include: cross country skiing, philosophy, backpacking, classic literature, and cryptocurrency.</p>
       <h2>Contact Info</h2>
       <p>email: apromislow@gmail.com</p>
       <p>github: https://github.com/apromislow</p>
